b04a2e9663f837cf8f61bb4be4e976132baae92b
Ayhan Dorman <dorman.ayhan@gmail.com>
2019-02-09 15:33:43 +0300

    Implemented path-finding.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2019 Ayhan Dorman
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 const   floorWidth = 250,
         tileWidth = 40,
         tileSize = 100,
@@ -11,55 +35,73 @@ const   floorWidth = 250,
             right: 6,
             downRight: 7
         };
-        // 3  4  5
-        // 2     6
-        // 1  0  7
-var canvas, screenWidth, screenHeight, mouse,
+var canvas, screenWidth, screenHeight, cursorPosition, 
+    currentButton = 3,
+    targetPart = 0, 
     processing = false,
     tiles = Array(tileSize),
     target = {
-        x: 100,
-        y: 100
-    };
+        x: 5,
+        y: 5
+    },
+    theWay = [{x: 5, y: 5}];
 
 class Soldier {
 
     constructor() {        
-        this.x = 100;
-        this.y = 100;
+        this.x = 220;
+        this.y = 200;
         this.counter = 0;
         this.direction = directions.down;
     }
     
     render = (context) => {
+        let whereToGo = theWay[targetPart] ? {
+            x: theWay[targetPart].x * tileWidth + 20,
+            y: theWay[targetPart].y * tileWidth
+        } : {x: this.x, y: this.y}
 
-        if (this.x != target.x || this.y != target.y) {
+        if (this.x != whereToGo.x || this.y != whereToGo.y) {
             this.counter = (this.counter + 1) % 36;
-            this.x += target.x == this.x ? 0 : target.x < this.x ? -1 : 1;
-            this.y += target.y == this.y ? 0 : target.y < this.y ? -1 : 1;
+            this.x += whereToGo.x == this.x ? 0 : whereToGo.x < this.x ? -1 : 1;
+            this.y += whereToGo.y == this.y ? 0 : whereToGo.y < this.y ? -1 : 1;
 
             switch(true) {
-                case (this.x > target.x && this.y == target.y): this.direction = directions.left; break;
-                case (this.x < target.x && this.y == target.y): this.direction = directions.right; break;
-                case (this.y > target.y && this.x == target.x): this.direction = directions.up; break;
-                case (this.y < target.y && this.x == target.x): this.direction = directions.down; break;
-                case (this.x > target.x && this.y > target.y): this.direction = directions.upLeft; break;
-                case (this.x < target.x && this.y < target.y): this.direction = directions.downRight; break;
-                case (this.x > target.x && this.y < target.y): this.direction = directions.downLeft; break;
-                case (this.x < target.x && this.y > target.y): this.direction = directions.upRight; break;
+                case (this.x > whereToGo.x && this.y == whereToGo.y): this.direction = directions.left; break;
+                case (this.x < whereToGo.x && this.y == whereToGo.y): this.direction = directions.right; break;
+                case (this.y > whereToGo.y && this.x == whereToGo.x): this.direction = directions.up; break;
+                case (this.y < whereToGo.y && this.x == whereToGo.x): this.direction = directions.down; break;
+                case (this.x > whereToGo.x && this.y > whereToGo.y): this.direction = directions.upLeft; break;
+                case (this.x < whereToGo.x && this.y < whereToGo.y): this.direction = directions.downRight; break;
+                case (this.x > whereToGo.x && this.y < whereToGo.y): this.direction = directions.downLeft; break;
+                case (this.x < whereToGo.x && this.y > whereToGo.y): this.direction = directions.upRight; break;
+            }
+        } else {
+            if (targetPart < theWay.length) {
+                targetPart++;
             }
         }
+        // <render shadow>
+        context.shadowBlur = 5;
+        context.shadowColor = "black";
+        context.fillStyle = "rgba(0,0,0,.3)";
+        context.beginPath();
+        context.ellipse(this.x, this.y + 19, 16, 8, 0, 0, 2 * Math.PI);
+        context.fill();
+        context.shadowBlur = 0;
+        // </render shadow>
         context.drawImage(soldierImage, Math.floor(this.counter / 6) * 69, this.direction * 96, 69, 96, this.x - 34.5, this.y - 48, 69, 96);
     };
 }
-
-// 69x96				
+			
 var soldierImage = new Image();
 soldierImage.src = "./assets/soldier.png";
 var backgroundImage = new Image();
 backgroundImage.src = "./assets/grass.png";
 var stoneImage = new Image();
 stoneImage.src = "./assets/stone.png";
+var targetImage = new Image();
+targetImage.src = "./assets/target.png";
 
 const setCanvasSize = () => {
     screenWidth = window.innerWidth;
@@ -68,18 +110,60 @@ const setCanvasSize = () => {
     canvas.height = screenHeight;
 }
 
+document.oncontextmenu = (e) => e.preventDefault();
+
 window.onresize = () => setCanvasSize();
 
 window.onload = () => {
     canvas = document.querySelector("canvas");
     context = canvas.getContext("2d");
     setCanvasSize();
-
-    canvas.onmousedown = (e) => {
-        target = {
-            x: e.offsetX,
-            y: e.offsetY
+    for (let i = 0; i <= tileSize; i++) {
+        tiles[i] = new Array(tileSize);
+        for (let j = 0; j <= tileSize; j++) {
+            tiles[i][j] = {
+                type: 0,
+                direction: 0
+            };
         }
+    }
+
+    markDirections = (x, y) => {
+        let found = false;
+        if (x > 0 && tiles[x - 1][y].type != 1 && tiles[x - 1][y].direction == 0) {
+            tiles[x - 1][y].direction = -4;
+            if (x - 1 == target.x && y == target.y) found = true;
+            progressing = true;
+        }
+        if (y > 0 && tiles[x][y - 1].type != 1 && tiles[x][y - 1].direction == 0) {
+            tiles[x][y - 1].direction = 2;
+            if (x == target.x && y - 1 == target.y) found = true;
+            progressing = true;
+        }
+        if (x < tileSize && tiles[x + 1][y].type != 1 && tiles[x + 1][y].direction == 0) {
+            tiles[x + 1][y].direction = 4;
+            if (x + 1 == target.x && y == target.y) found = true;
+            progressing = true;
+        }
+        if (y < tileSize && tiles[x][y + 1].type != 1 && tiles[x][y + 1].direction == 0) {
+            tiles[x][y + 1].direction = -2;
+            if (x == target.x && y + 1 == target.y) found = true;
+            progressing = true;
+        }
+        return found;
+    }
+
+    canvas.onmousemove = (e) => {
+        cursorPosition = {
+            x: parseInt(e.clientX / tileWidth),
+            y: parseInt(e.clientY / tileWidth)
+        };
+        if (currentButton == 2) {
+            tiles[parseInt(e.clientX / tileWidth)][parseInt(e.clientY / tileWidth)].type = 1;
+        }
+    }
+
+    resetDirections = () => {
         for (let i = 0; i <= tileSize; i++) {
             for (let j = 0; j <= tileSize; j++) {
                 tiles[i][j].direction = 0;
@@ -87,23 +171,81 @@ window.onload = () => {
         }
     }
 
-    drawTiles = () => {
-        context.strokeStyle = '#888';
-        context.beginPath();
-        for (let i = 0; i <= tileSize; i++) {
-            tiles[i] = new Array(tileSize);
-            for (let j = 0; j <= tileSize; j++) {
-                tiles[i][j] = {
-                    type: 0,
-                    direction: 0
-                };
+    canvas.onmousedown = (e) => {
+        currentButton = e.button;
+
+        if (currentButton == 2 && tiles[cursorPosition.x][cursorPosition.y].type == 0) {
+            tiles[cursorPosition.x][cursorPosition.y].type = 1;
+        } else if (currentButton == 0) {
+            resetDirections();
+            target = {
+                x: cursorPosition.x,
+                y: cursorPosition.y
             }
-            context.moveTo(tileWidth * i, 0);
-            context.lineTo(tileWidth * i, screenHeight);
-            context.moveTo(0, tileWidth * i);
-            context.lineTo(screenWidth, tileWidth * i);
+
+            // find
+            var found = false;
+            var initialSearch = false;
+            progressing = true;
+            
+            while (!found && progressing) {
+                if (!initialSearch) {
+                    found = markDirections(parseInt(soldier.x / tileWidth), parseInt(soldier.y / tileWidth));
+                    initialSearch = true;
+                    } else {
+                    progressing = false;
+                    for (let i = 0; i < tileSize; i++) {
+                        for (let j = 0; j < tileSize; j++) {
+                            if (tiles[i][j].direction != 0) {
+                                found = markDirections(i, j);
+                                if (found) {
+                                    break;
+                                }
+                            }            
+                        }
+                        if (found) break;
+                    }
+                }
+            }
+            if (found) {
+                targetPart = 0;
+                let currentLocation = {
+                    x: target.x,
+                    y: target.y
+                }
+                let soldierLocation = {
+                    x: parseInt(soldier.x / tileWidth),
+                    y: parseInt(soldier.y / tileWidth)
+                }
+                theWay = [];            
+                while (currentLocation.x != soldierLocation.x || currentLocation.y != soldierLocation.y) {
+                    switch (tiles[currentLocation.x][currentLocation.y].direction) {
+                        case -4: ++currentLocation.x; break;
+                        case  1: ++currentLocation.x; ++currentLocation.y; break;
+                        case  2: ++currentLocation.y; break;
+                        case  3: --currentLocation.x; ++currentLocation.y; break;
+                        case  4: --currentLocation.x; break;
+                        case -1: --currentLocation.x; --currentLocation.y; break;
+                        case -2: --currentLocation.y; break;
+                        case -3: ++currentLocation.x; --currentLocation.y; break;          
+                    }
+                    theWay.push({x: currentLocation.x, y: currentLocation.y});
+                }
+                theWay = theWay.reverse();
+            }
         }
-        context.stroke();
+    }
+
+    canvas.onmouseup = () => currentButton = 3;
+
+    drawTiles = () => {
+        for (let i = 0; i <= tileSize; i++) {
+            for (let j = 0; j <= tileSize; j++) {
+                if (tiles[i][j].type == 1) {
+                    context.drawImage(stoneImage, i * tileWidth, j * tileWidth, tileWidth, tileWidth);
+                }
+            }
+        }
     }
 
     update = () => {
@@ -113,6 +255,7 @@ window.onload = () => {
             }
         }
         drawTiles();
+        context.drawImage(targetImage, target.x * tileWidth, target.y * tileWidth - 15, tileWidth, tileWidth);
         soldier.render(context);
     }
 
