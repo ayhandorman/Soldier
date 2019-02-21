32f0a582517f8994e6a15a55107d3e57e4550810
Ayhan Dorman <dorman.ayhan@gmail.com>
2019-02-22 00:18:20 +0300

    Refactored.

--- src/entities/Monster.js
+++ src/entities/Monster.js
@@ -16,27 +16,28 @@ export class Monster {
     }
 
     render = (screen, renderScope, soldier) => {
-        if (this.x == this.target.x * this.world.tileWidth && this.y == this.target.y * this.world.tileWidth && this.hp == this.maxHP) {
+        let world = this.world;
+        if (this.x == this.target.x * world.tileWidth && this.y == this.target.y * world.tileWidth && this.hp == this.maxHP) {
             let availableDirections = [];
-            if (this.x / this.world.tileWidth > 0 && !this.world.tiles[this.x / this.world.tileWidth - 1][this.y / this.world.tileWidth].blocking) availableDirections.push(this.world.directions.left);
-            if (this.x / this.world.tileWidth < this.world.size && !this.world.tiles[this.x / this.world.tileWidth + 1][this.y / this.world.tileWidth].blocking) availableDirections.push(this.world.directions.right);
-            if (this.y / this.world.tileWidth > 0 && !this.world.tiles[this.x / this.world.tileWidth][this.y / this.world.tileWidth - 1].blocking) availableDirections.push(this.world.directions.up);
-            if (this.y / this.world.tileWidth < this.world.size && !this.world.tiles[this.x / this.world.tileWidth][this.y / this.world.tileWidth + 1].blocking) availableDirections.push(this.world.directions.down);
+            if (this.x / world.tileWidth > 0 && !world.tiles[this.x / world.tileWidth - 1][this.y / world.tileWidth].blocking) availableDirections.push(world.directions.left);
+            if (this.x / world.tileWidth < world.size && !world.tiles[this.x / world.tileWidth + 1][this.y / world.tileWidth].blocking) availableDirections.push(world.directions.right);
+            if (this.y / world.tileWidth > 0 && !world.tiles[this.x / world.tileWidth][this.y / world.tileWidth - 1].blocking) availableDirections.push(world.directions.up);
+            if (this.y / world.tileWidth < world.size && !world.tiles[this.x / world.tileWidth][this.y / world.tileWidth + 1].blocking) availableDirections.push(world.directions.down);
             this.direction = availableDirections[Math.floor(Math.random() * availableDirections.length)];
             switch(this.direction) {
-                case this.world.directions.left: this.target.x--; break;
-                case this.world.directions.right: this.target.x++; break;
-                case this.world.directions.up: this.target.y--; break;
-                case this.world.directions.down: this.target.y++; break;
+                case world.directions.left: this.target.x--; break;
+                case world.directions.right: this.target.x++; break;
+                case world.directions.up: this.target.y--; break;
+                case world.directions.down: this.target.y++; break;
             }
         } else {
             this.counter = (this.counter + 1) % 30;
-            this.x = this.x > this.target.x * this.world.tileWidth ? this.x - 1 : this.x + 1;
-            this.y = this.y > this.target.y * this.world.tileWidth ? this.y - 1 : this.y + 1;
+            this.x = this.x > this.target.x * world.tileWidth ? this.x - 1 : this.x + 1;
+            this.y = this.y > this.target.y * world.tileWidth ? this.y - 1 : this.y + 1;
         }
         let position = {
-            x: this.x / this.world.tileWidth,
-            y: this.y / this.world.tileWidth
+            x: this.x / world.tileWidth,
+            y: this.y / world.tileWidth
         }
         if (position.x >= renderScope.x1 - 1 && 
             position.x <= renderScope.x2 + 1 &&
@@ -46,7 +47,7 @@ export class Monster {
                 x: this.x - soldier.x + screen.width / 2,
                 y: this.y - soldier.y + screen.height / 2
             }
-            let context = this.world.context;
+            let context = world.context;
             context.drawImage(this.sprite, Math.floor(this.counter / 10) * 48, this.direction * 48, 48, 48, monsterPosition.x - 4, monsterPosition.y - 15, 48, 48);
 
             context.font = "14px Arial";
