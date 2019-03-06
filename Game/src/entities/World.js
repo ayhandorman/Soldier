e8292d8d9a0c01fbd298c8918863c93f063c943e
Ayhan Dorman <dorman.ayhan@gmail.com>
2019-03-06 22:32:43 +0300

    Added new tiles and map.

--- Game/src/entities/World.js
+++ Game/src/entities/World.js
@@ -5,7 +5,7 @@ export class World {
     constructor() {
         this.size = 0;
         this.tileWidth = 40;
-        this.tileTypes = 104;
+        this.tileTypes = 112;
         this.directions = {
             down: 0,
             downLeft: 1,
@@ -18,9 +18,9 @@ export class World {
         };
         this.blockingTypes = [  
                                 1, 6, 15, 16, 17, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
-                                31, 35, 36, 38, 39, 40, 41, 45, 46, 47, 48, 49, 52, 53, 54, 
+                                31, 34, 35, 36, 38, 39, 40, 41, 45, 46, 47, 48, 49, 52, 53, 54, 
                                 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 70, 
-                                71, 99, 100, 101, 102, 103
+                                71, 99, 100, 101, 102, 103, 105, 106, 107, 108, 109, 110, 111
                             ];
         this.images = new Array(this.tileTypes);
         this.tiles = [];
